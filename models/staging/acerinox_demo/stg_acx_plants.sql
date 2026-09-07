76ef9420bfbdd27529ada70032407b955425c21a
Alvaro Belloso <alvaro.belloso@ibm.com>
2026-09-07 16:33:15 +0000

    changed syntax plants sql

--- models/staging/acerinox_demo/stg_acx_plants.sql
+++ models/staging/acerinox_demo/stg_acx_plants.sql
@@ -1,25 +1,13 @@
-with
-
-source as (
-
-    select * from {{ source('acerinox_demo', 'plants') }}
-
-),
-
-renamed as (
-    select
-        plantid       as plant_id,
-        plantref      as plant_ref,
-        name          as plant_name,
-        location,
-        country,
-        lat           as latitude,
-        lng           as longitude,
-        capacity_tons_per_year,
-        founded_year
-        --planned_maintenance_window
-        -- omit internal metadata columns if present
-    from source
-)
-
-select * from renamed
+select 
+    plantid       as plant_id,
+    plantref      as plant_ref,
+    name          as plant_name,
+    location,
+    country,
+    lat           as latitude,
+    lng           as longitude,
+    capacity_tons_per_year,
+    founded_year
+    --planned_maintenance_window
+    -- omit internal metadata columns if present
+from {{ source('acerinox_demo', 'plants') }}
